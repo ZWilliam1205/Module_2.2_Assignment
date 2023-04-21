62337a5836bc3e1276dea374690587914d3aa092
ZWilliam1205 <127437537+ZWilliam1205@users.noreply.github.com>
2023-04-20 20:21:29 -0700

    Initial commit

---
+++ src/Face_card.java
@@ -0,0 +1,9 @@
+public class Face_card extends Card {
+    FaceType face;
+
+    public Face_card(Suits suitType , FaceType face) {
+        this.suitType = suitType;
+        this.face = face;
+    }
+
+}
---
+++ src/Number_card.java
@@ -0,0 +1,8 @@
+public class Number_card extends Card {
+    int cardNum;
+
+    public Number_card(int cardNum , Suits suitType){
+        this.cardNum = cardNum;
+        this.suitType = suitType;
+    }
+}
